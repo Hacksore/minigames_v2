7cceee733b2373b0098ad6d005556857a092ba74
Xobile <knightroonik@gmail.com>
2020-04-06 01:12:33 +1000

    fixed sniper spawning probably

--- fluffy_microgames/gamemode/modifiers/takecover.lua
+++ fluffy_microgames/gamemode/modifiers/takecover.lua
@@ -1,18 +1,25 @@
-MOD.Name = 'Ambush'
-MOD.Region = 'empty'
-
-MOD.SurviveValue = 4
+MOD.Name = 'Take Cover!'
+MOD.ScoringPane = true
 
 local function spawnSnipers()
-    local number = GAMEMODE:PlayerScale(0.5, 10, 15)
+    local number = GAMEMODE:PlayerScale(0.75, 2, 16)
     local positions = GAMEMODE:GetRandomLocations(number, 'crate')
 
     for i=1,number do
         local pos = positions[i] + Vector(0, 0, 32)
         local ent = ents.Create("npc_sniper") 
-        ent:SetPos(pos)
-        ent:Spawn()
+        sniper:SetPos(pos)
+        sniper:Spawn()
+        sniper:SetMaxHealth(10)
+        sniper:SetHealth(10)
     end
+end
+
+function MOD:OnNPCKilled(npc, attacker, inflictor)
+    if not attacker:IsPlayer() then return end
+    if npc:GetClass() != "npc_sniper" then return end
+
+    attacker:AddMScore(1)
 end
 
 function MOD:Initialize()
@@ -21,8 +28,9 @@ function MOD:Initialize()
 end
 
 function MOD:Loadout(ply)
-    ply:SetMaxHealth(30)
-    ply:SetHealth(30)
+    ply:SetMaxHealth(10)
+    ply:SetHealth(10)
+    ply:Give("weapon_357")
 end
     
 function MOD:EntityTakeDamage(ent, dmg)
